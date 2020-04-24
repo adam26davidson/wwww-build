79809a636389e8dbdfb4bc7b251cf6a4512195b4
Adam Davidson <davidson.557@osu.edu>
2020-04-24 16:15:19 -0400

    Add files via upload

---
+++ precache-manifest.95eeba115e8604d9143285652a59bb50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df42e922c05207ac6beda6b891b44469",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb0519fe126be2ab09d4",
+    "url": "/static/css/main.4732850e.chunk.css"
+  },
+  {
+    "revision": "9d5bba3a669a5af238ff",
+    "url": "/static/js/2.bb6b6040.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.bb6b6040.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb0519fe126be2ab09d4",
+    "url": "/static/js/main.9cfb15e3.chunk.js"
+  },
+  {
+    "revision": "c0530a4ab589fee39e5f",
+    "url": "/static/js/runtime-main.ed885276.js"
+  }
+]);
